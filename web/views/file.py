5f0b58c9a29029c16ca034b7e344864fd2dd10ee
bgstiandao <1832341080@qq.com>
2026-02-26 16:03:42 +0800

    文件管理-编辑文件夹

--- web/views/file.py
+++ web/views/file.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.forms import model_to_dict
+
 from web.forms.file import FileModelForm
 from web import models
 
@@ -14,14 +16,45 @@ def file(request,project_id):
     if folder_id.isdecimal():
         parent_object = models.FileRepository.objects.filter(id=int(folder_id), file_type=2,
                                                              project=request.tracer.project).first()
-
+    #GET 查看页面
     if request.method == 'GET':
+
+        breadcrumb_list = []
+        parent = parent_object
+        while parent:
+            #breadcrumb_list.insert(0, {'id':parent.id, 'name':parent.name})
+            breadcrumb_list.insert(0, model_to_dict(parent,['id','name']))
+            parent = parent.parent
+
+        #当前目录下所有文件 & 文件夹获取到即可
+        queryset = models.FileRepository.objects.filter(project=request.tracer.project)
+        if parent_object:
+            #进入某目录
+            file_object_list = queryset.filter(parent=parent_object).order_by('-file_type')
+        else:
+            #根目录
+            file_object_list = queryset.filter(parent__isnull=True).order_by('-file_type')
+
         form = FileModelForm(request,parent_object)
-        return render(request,'file.html',{'form':form})
+        context = {
+            'form': form,
+            'file_object_list': file_object_list,
+            'breadcrumb_list': breadcrumb_list,
+        }
+        return render(request,'file.html',context)
 
 
-    #添加文件夹
-    form = FileModelForm(request,parent_object,request.POST)
+    #POST 添加文件夹 & 文件夹的修改
+    fid = request.POST.get('fid','')
+    edit_object = None
+    if fid.isdecimal():
+        edit_object = models.FileRepository.objects.filter(id=int(fid), file_type=2,project=request.tracer.project).first()
+
+    if edit_object:
+        form = FileModelForm(request, parent_object, request.POST,instance=edit_object)
+    else:
+        form = FileModelForm(request,parent_object,request.POST)
+
     if form.is_valid():
         form.instance.project = request.tracer.project
         form.instance.file_type = 2
