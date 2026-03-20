0ae498c3fef6417d4addc711677f5daef610678e
bgstiandao <1832341080@qq.com>
2026-03-20 16:20:06 +0800

    bootstrap-select插件，ModelForm里面添加属性

--- web/forms/issues.py
+++ web/forms/issues.py
@@ -6,3 +6,7 @@ class IssuesModelForm(BootstrapForm,forms.ModelForm):
     class Meta:
         model = models.Issues
         exclude = ['project','creator','create_datetime','last_update_datetime']
+        widgets = {
+            'assign':forms.Select(attrs={'class':'selectpicker','data-live-search':'true'}),
+            'attention':forms.SelectMultiple(attrs={'class':'selectpicker','data-live-search':'true','data-actions-box':'true'}),
+        }
